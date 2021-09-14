f50589f45acba612b6986e4af855dbff2d22df3a
Tharindu Akalaka <tharindu.thisun123@gmail.com>
2021-09-14 22:01:57 +0530

    lambda expression

--- src/main/java/com/example/sample/services/components/SampleComponent.java
+++ src/main/java/com/example/sample/services/components/SampleComponent.java
@@ -1,12 +1,8 @@
 package com.example.sample.services.components;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
 @FunctionalInterface
-@Component
 public interface SampleComponent {
-    @Bean
     public void execute();
 
     public static String cal(){
